5bba3970262a088c52a2db4769f52e59f311e0b3
JonhDoel <yuzko@ukr.net>
2025-01-19 18:19:20 +0200

    добавим кнопки

---
+++ buttons.py
@@ -0,0 +1,25 @@
+import logging
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
+from message_utils import send_message_with_retries
+from weather import get_weather_update
+from currency import get_currency_rate
+
+logger = logging.getLogger(__name__)
+
+async def show_menu(update, context):
+    keyboard = [
+        [
+            InlineKeyboardButton("Погода", callback_data='weather'),
+            InlineKeyboardButton("Курсы валют", callback_data='currency')
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Выберите опцию:', reply_markup=reply_markup)
+
+async def button(update, context):
+    query = update.callback_query
+    await query.answer()
+    if query.data == 'weather':
+        await get_weather_update(update, context)
+    elif query.data == 'currency':
+        await get_currency_rate(update, context)
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import logging
-from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
 from config import TELEGRAM_TOKEN
-from weather import get_weather_update, send_weather_update
-from message_utils import send_message_with_retries
-from currency import get_currency_rate
+from weather import get_weather_update
+from buttons import show_menu, button
 
 # Настройка логирования
 logging.basicConfig(
@@ -17,27 +16,7 @@ logger = logging.getLogger(__name__)
 async def start(update: Update, context):
     logger.info(f"Команда /start получена от пользователя {update.effective_user.id}")
     bot = context.bot
-    await send_message_with_retries(bot, update.effective_chat.id, "Привет! Я бот для получения погоды и курсов валют. Просто введи название города на украинском языке, чтобы узнать погоду. 😃")
-
-# Функция для инициализации кнопок
-async def show_buttons(update: Update, context):
-    keyboard = [
-        [
-            InlineKeyboardButton("Погода", callback_data='weather'),
-            InlineKeyboardButton("Курсы валют", callback_data='currency')
-        ]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text('Выберите опцию:', reply_markup=reply_markup)
-
-# Обработчик нажатия кнопок
-async def button(update: Update, context):
-    query = update.callback_query
-    await query.answer()
-    if query.data == 'weather':
-        await get_weather_update(update, context)
-    elif query.data == 'currency':
-        await get_currency_rate(update, context)
+    await send_message_with_retries(bot, update.effective_chat.id, "Привет! Я бот для получения погоды и курсов валют. Просто введи название города на украинском языке, чтобы узнать погоду. 😃\nДля просмотра меню нажмите /menu.")
 
 def main():
     # Создание бота и добавление обработчиков
@@ -46,8 +25,8 @@ def main():
     # Обработчик команды /start
     application.add_handler(CommandHandler("start", start))
 
-    # Обработчик команды /buttons
-    application.add_handler(CommandHandler("buttons", show_buttons))
+    # Обработчик команды /menu
+    application.add_handler(CommandHandler("menu", show_menu))
 
     # Обработчик нажатий кнопок
     application.add_handler(CallbackQueryHandler(button))
@@ -58,5 +37,5 @@ def main():
     # Запуск бота
     application.run_polling()
 
-if __name__ == "main":
+if __name__ == "__main__":
     main()
