5f5d933f87adbad57bdc06ae7599818253d1c668
JonhDoel <yuzko@ukr.net>
2025-01-20 00:14:17 +0200

    Теперь main.py будет более компактным, а функциональность, связанная с уведомлениями и автообновлениями, вынесена в отдельный модуль notifications.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
-from datetime import time, datetime, timedelta
 from pytz import timezone
 from config import TELEGRAM_TOKEN
 from user_data import save_user_data, load_user_data
 from menu import show_menu, button
 from message_utils import send_message_with_retries
 from utils import request_city
-from weather import get_weather
+from notifications import send_notification, send_daily_weather_update, schedule_daily_weather_update, schedule_auto_update
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levellevel)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
@@ -84,43 +83,6 @@ async def save_city(update: Update, context):
     else:
         await button(update, context)
     schedule_auto_update(context, update.effective_chat.id)
-
-async def send_notification(context: CallbackContext):
-    job = context.job
-    user_id = job.data['user_id']
-    await send_message_with_retries(context.bot, job.data['chat_id'], "Вы будете получать прогноз погоды каждое утро в 8 часов.")
-    # Обновляем JSON, чтобы уведомление больше не отправлялось
-    user_data = load_user_data(user_id)
-    user_data['notified'] = True
-    save_user_data(user_id, user_data['city'], notified=True)
-
-async def send_daily_weather_update(context: CallbackContext):
-    job = context.job
-    user_id = job.data['user_id']
-    user_data = load_user_data(user_id)
-    if user_data and user_data.get('city'):
-        city = user_data['city']
-        weather_info = await get_weather(city)
-        await send_message_with_retries(context.bot, job.data['chat_id'], weather_info)
-
-def schedule_daily_weather_update(context: CallbackContext, chat_id, user_timezone):
-    target_time = time(hour=8, minute=0, tzinfo=user_timezone)
-    context.job_queue.run_daily(send_daily_weather_update, target_time, data={'chat_id': chat_id, 'user_id': chat_id})
-
-async def auto_update(context: CallbackContext):
-    job = context.job
-    city = job.data.get('city')
-    chat_id = job.data.get('chat_id')
-    if city:
-        weather_info = await get_weather(city)
-        await send_message_with_retries(context.bot, chat_id, weather_info)
-
-def schedule_auto_update(context: CallbackContext, chat_id):
-    job_queue = context.job_queue
-    user_data = load_user_data(chat_id)
-    city = user_data.get('city') if user_data else None
-    if city:
-        job_queue.run_repeating(auto_update, interval=7200, first=7200, data={'chat_id': chat_id, 'city': city})
 
 def main():
     logger.info("Запуск бота...")
